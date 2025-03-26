2288d9ef10a9144f98ec48de61d7c12ef00b68b0
Marisa Kirisame <marisa@cs.utah.edu>
2025-03-26 01:21:55 -0600

    oopsla

--- eval/eval.py
+++ eval/eval.py
@@ -108,8 +108,8 @@ with doc:
             ax1.plot([min_value, max_value], [min_value, max_value], color="black")
             ax1.set_xscale('log')
             ax1.set_yscale('log')
-            ax1.set_xlabel("Computation cycles (from-scratch)")
-            ax1.set_ylabel("Computation cycles (MALCOM)")
+            ax1.set_xlabel("Cycles (from-scratch)")
+            ax1.set_ylabel("Cycles (incremental)")
             #ax1.set_xlim(min_value / 2, max_value * 2)
             #ax1.set_ylim(min_value / 2, max_value * 2)
 
@@ -163,7 +163,7 @@ with doc:
             
     compare([], [(x[0], y[0]) for (x, y) in times])
     compare([(x[1], y[1]) for (x, y) in times], [])
-    # compare([(x[0] + x[1], y[0] + y[1]) for (x, y) in times], [])
+    compare([(x[0] + x[1], y[0] + y[1]) for (x, y) in times], [])
     compare([(x[1], y[1]) for (x, y) in times], [(x[0], y[0]) for (x, y) in times])
 
     data = []
--- eval/eval.re
+++ eval/eval.re
@@ -12,6 +12,9 @@ open Hazelnut_lib.Actions_random;
 open Hazelnut_lib.Update;
 open Hazelnut_lib.Marking;
 open Ocaml_intrinsics;
+open Gc
+
+let _ = Gc.set { ...Gc.get(), space_overhead: 1200 };
 
 let random_element = x => List.nth_exn(x, Random.int(List.length(x)));
 
@@ -963,6 +966,8 @@ let should_skip = act =>
   switch (act) {
   | SDown(_)
   | SUp => true
+  | Down(_)
+  | Up => true
   | _ => false
   };
 let init_eval_state = () => {istate: initial_state(), estate: (Hole, [])};
